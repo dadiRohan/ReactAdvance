904f52bbe83fca5dc79048f970acb1fb68989eb4
Rohan Sable <39899398+dadiRohan@users.noreply.github.com>
2024-08-10 13:44:29 +0530

    Filter with useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "./Card";
-import {restList} from "../utils/mockdata";
-
+import {restList as restListJS } from "../utils/mockdata";
 
 const Body = () => {
+
+    const [restList,setList] = useState(restListJS); 
+
     return (
         <div className="main-body">
             
             <button className="filter-btn" onClick={()=>{
                 console.log('Filter button clicked');
+                const filterRestList = restList.filter(
+                    (res) => res.data.rate > 4
+                );
+                setList(filterRestList);
+                console.log(filterRestList);
             }}>Top Rated Restaurants</button>
 
             <div className="search-box">
--- src/utils/mockdata.js
+++ src/utils/mockdata.js
@@ -9,6 +9,7 @@ export const restList = [
             city : "kalyan",
             cusine : "Burger, Rolls, French fries",
             rating : "4.5 stars",
+            rate : 4.5,
             photo : "https://cdn.vectorstock.com/i/500p/22/07/burger-icon-isolated-flat-cartoon-sandwich-vector-15262207.jpg"
         }
     },
@@ -22,6 +23,7 @@ export const restList = [
             city : "Dombivli",
             cusine : "Thali, North & South indian, Soft Drinks and bevarage",
             rating : "3.2 stars",
+            rate : 3.2,
             photo : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyZfxNY8r2-a60xbloK3hQwgqV9ezYFqJlzg&s"
         }
     },
@@ -35,7 +37,36 @@ export const restList = [
             city : "Manpada",
             cusine : "Indian Cusines, Chinese, Mexican, Italian, Bear and Cocktails",
             rating : "4.9 stars",
+            rate : 4.9,
             photo : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJl0nGCPWmVgaoLBHNR3lxTHy0m9OqaFwlGg&s"
+        }
+    },
+    {
+        type : "restaurant",
+        data : {
+            type: "F",
+            id : 341313,
+            name : "Jombo King",
+            uuid : "13131311212",
+            city : "kalyan",
+            cusine : "Burger, Rolls, French fries",
+            rating : "3.5 stars",
+            rate : 3.5,
+            photo : "https://cdn.vectorstock.com/i/500p/22/07/burger-icon-isolated-flat-cartoon-sandwich-vector-15262207.jpg"
+        }
+    },
+    {
+        type : "restaurant",
+        data : {
+            type: "F",
+            id : 342123,
+            name : "SVG",
+            uuid : "13131311212",
+            city : "kalyan",
+            cusine : "Burger, Rolls, French fries",
+            rating : "2.1 stars",
+            rate : 2.1,
+            photo : "https://cdn.vectorstock.com/i/500p/22/07/burger-icon-isolated-flat-cartoon-sandwich-vector-15262207.jpg"
         }
     }
 ] 
