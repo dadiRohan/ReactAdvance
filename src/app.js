3fc1b78449d706c93046082babaeb3b0f035f0af
Rohan Sable <39899398+dadiRohan@users.noreply.github.com>
2024-08-13 12:39:39 +0530

    App changes with shimmer

--- src/app.js
+++ src/app.js
@@ -2,15 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
+import {NormalState,UseStateVar,NormalEffect,Dashed} from './components/Hook';
 
 
 // React Component Composition //
 const Reactcomponent = () => (
     <div id="container">
-        <Header/>
+         <Header/>
         <h2 className="heading">This is Order Platform</h2>
-        <Body/>
+        <Body/> 
+        
+        {/* <NormalState/>
+        <UseStateVar/>
+        <NormalEffect/>     */}
+
+        {/* <Dashed/> */}
     </div>
 );
 // console.log(Reactcomponent);
