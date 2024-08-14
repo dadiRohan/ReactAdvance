c868208b354dbaa87d3c3e96174775d5cf7340ed
Rohan Sable <39899398+dadiRohan@users.noreply.github.com>
2024-08-14 13:19:55 +0530

    Try Catch exception managed for Live API

--- src/utils/mockdata.js
+++ src/utils/mockdata.js
@@ -71,3 +71,84 @@ export const restList = [
     }
 ] 
 
+export const RestAdvanceList = [
+    {
+        "text": "Thane Briyani Center",
+        "highlightedText": "{{Thane}} Briyani Center",
+        "type": "RESTAURANT",
+        "suggestionType": "SUGGESTION_TYPE_LOW_CONFIDENCE",
+        "marketplace": {
+          "marketplaceId": "SWIGGY",
+          "businessLineId": "FOOD"
+        },
+        "tagToDisplay": "Restaurant",
+        "tagToDisplayColor": "#686B78",
+        "cloudinaryId": "disabled_restaurant_default_image",
+        "communication": {
+          "text": "Oops! This restaurant has shut down",
+          "textColor": "negative"
+        },
+        "metadata": "{\"type\":\"RESTAURANT\",\"data\":{\"parentId\":293432,\"primaryRestaurantId\":489344,\"cloudinaryId\":\"disabled_restaurant_default_image\",\"brandId\":293432,\"enabled_flag\":0},\"businessCategory\":\"SWIGGY_FOOD\",\"displayLabel\":\"Restaurant\"}",
+        "cta": {
+          "text": "Thane Briyani Center",
+          "type": "DEEPLINK",
+          "link": "swiggy://explore?query=Thane%20Briyani%20Center&metadata=%7B%22type%22%3A%22RESTAURANT%22%2C%22data%22%3A%7B%22parentId%22%3A293432%2C%22primaryRestaurantId%22%3A489344%2C%22cloudinaryId%22%3A%22disabled_restaurant_default_image%22%2C%22brandId%22%3A293432%2C%22enabled_flag%22%3A0%7D%2C%22businessCategory%22%3A%22SWIGGY_FOOD%22%2C%22displayLabel%22%3A%22Restaurant%22%7D&marketplace=%7B%22marketplaceId%22%3A%22SWIGGY%22%2C%22businessLineId%22%3A%22FOOD%22%7D"
+        },
+        "category": "Food",
+        "categoryColor": "#686B78",
+        "subCategory": "Restaurant",
+        "subCategoryColor": "#686B78",
+        "restaurantId": 0,
+        "disabled": true
+      },
+      {
+        "text": "Thane Canteen",
+        "highlightedText": "{{Thane}} Canteen",
+        "type": "RESTAURANT",
+        "suggestionType": "SUGGESTION_TYPE_LOW_CONFIDENCE",
+        "marketplace": {
+          "marketplaceId": "SWIGGY",
+          "businessLineId": "FOOD"
+        },
+        "tagToDisplay": "Restaurant",
+        "tagToDisplayColor": "#686B78",
+        "cloudinaryId": "hkmoeuhoqdceqpt6npt4",
+        "metadata": "{\"type\":\"RESTAURANT\",\"data\":{\"parentId\":204360,\"primaryRestaurantId\":258933,\"cloudinaryId\":\"hkmoeuhoqdceqpt6npt4\",\"brandId\":204360,\"enabled_flag\":1},\"businessCategory\":\"SWIGGY_FOOD\",\"displayLabel\":\"Restaurant\"}",
+        "cta": {
+          "text": "Thane Canteen",
+          "type": "DEEPLINK",
+          "link": "swiggy://explore?query=Thane%20Canteen&metadata=%7B%22type%22%3A%22RESTAURANT%22%2C%22data%22%3A%7B%22parentId%22%3A204360%2C%22primaryRestaurantId%22%3A258933%2C%22cloudinaryId%22%3A%22hkmoeuhoqdceqpt6npt4%22%2C%22brandId%22%3A204360%2C%22enabled_flag%22%3A1%7D%2C%22businessCategory%22%3A%22SWIGGY_FOOD%22%2C%22displayLabel%22%3A%22Restaurant%22%7D&marketplace=%7B%22marketplaceId%22%3A%22SWIGGY%22%2C%22businessLineId%22%3A%22FOOD%22%7D"
+        },
+        "category": "Food",
+        "categoryColor": "#686B78",
+        "subCategory": "Restaurant",
+        "subCategoryColor": "#686B78",
+        "restaurantId": 0,
+        "disabled": false
+      },
+      {
+        "text": "INDO CHINESE THANE",
+        "highlightedText": "INDO CHINESE {{THANE}}",
+        "type": "RESTAURANT",
+        "suggestionType": "SUGGESTION_TYPE_LOW_CONFIDENCE",
+        "marketplace": {
+          "marketplaceId": "SWIGGY",
+          "businessLineId": "FOOD"
+        },
+        "tagToDisplay": "Restaurant",
+        "tagToDisplayColor": "#686B78",
+        "cloudinaryId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/29/22d6f80a-6769-4a4d-9526-54b893aa909f_667248 SS.jpg",
+        "metadata": "{\"type\":\"RESTAURANT\",\"data\":{\"parentId\":524220,\"primaryRestaurantId\":667248,\"cloudinaryId\":\"RX_THUMBNAIL/IMAGES/VENDOR/2024/6/29/22d6f80a-6769-4a4d-9526-54b893aa909f_667248 SS.jpg\",\"brandId\":524220,\"dishFamilyId\":\"846613\",\"enabled_flag\":1},\"businessCategory\":\"SWIGGY_FOOD\",\"displayLabel\":\"Restaurant\"}",
+        "cta": {
+          "text": "INDO CHINESE THANE",
+          "type": "DEEPLINK",
+          "link": "swiggy://explore?query=INDO%20CHINESE%20THANE&metadata=%7B%22type%22%3A%22RESTAURANT%22%2C%22data%22%3A%7B%22parentId%22%3A524220%2C%22primaryRestaurantId%22%3A667248%2C%22cloudinaryId%22%3A%22RX_THUMBNAIL%2FIMAGES%2FVENDOR%2F2024%2F6%2F29%2F22d6f80a-6769-4a4d-9526-54b893aa909f_667248%20SS.jpg%22%2C%22brandId%22%3A524220%2C%22dishFamilyId%22%3A%22846613%22%2C%22enabled_flag%22%3A1%7D%2C%22businessCategory%22%3A%22SWIGGY_FOOD%22%2C%22displayLabel%22%3A%22Restaurant%22%7D&marketplace=%7B%22marketplaceId%22%3A%22SWIGGY%22%2C%22businessLineId%22%3A%22FOOD%22%7D"
+        },
+        "category": "Food",
+        "categoryColor": "#686B78",
+        "subCategory": "Restaurant",
+        "subCategoryColor": "#686B78",
+        "restaurantId": 0,
+        "disabled": false
+      }
+];
