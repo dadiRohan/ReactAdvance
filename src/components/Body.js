534e921e6d8e9e52fef806164599c1adfe8fb74e
Rohan Sable <39899398+dadiRohan@users.noreply.github.com>
2024-08-14 13:19:20 +0530

    Try Catch exception managed for Live API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,45 @@
 import React, {useEffect, useState} from "react";
 import Card from "./Card";
-import {restList as restListJS } from "../utils/mockdata";
+import {restList as restListJS,RestAdvanceList } from "../utils/mockdata";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
 
-    const [restList,setList] = useState(restListJS); 
+    const [restList,setList] = useState(0); 
+  
+    const fetchData = async () => {
+        try {
+            const data = await fetch("https://www.swiggy.com/dapi/restaurants/search/suggest?lat=19.120842&lng=72.9250169&str=Thane");
+
+            const json = await data.json();
+
+            setList(json?.data?.suggestions)
+            
+        } catch (error) {
+            // console.log(error)
+            const json = RestAdvanceList;
+            console.log('Data:');
+            console.log(json);
+            
+            setList(json)
+        }    
+    };
+
+        
 
     useEffect(() => {
-        fetchData();
-    },[])
+        
+        const timer = setTimeout(()=>{
+            // setList(RestAdvanceList);
 
-    const fetchData = async () => {
-        const data = await fetch(
-            "https://www.swiggy.com/dapi/restaurants/search/suggest?lat=19.120842&lng=72.9250169&str=kalyan",{mode: 'no-cors'}
-        );
+            fetchData();    
 
-        const json = await data.json();
+            console.log('SetTimeout Timer');
+        },3000);
+        return () => clearTimeout(timer);
 
-        console.log(json);
+    },[]);  
 
-        setList(json?.data?.suggestions)
-
-    };
 
     if(restList === 0){
         return <Shimmer />
@@ -32,25 +49,27 @@ const Body = () => {
         <div className="main-body">
             
             <button className="filter-btn" onClick={()=>{
-                //console.log('Filter button clicked');
+                
                 const filterRestList = restList.filter(
-                    (res) => res.data.rate > 4
+                    (res) => res?.data?.rate > 4
                 );
                 setList(filterRestList);
-                //console.log(filterRestList);
+                
             }}>Top Rated Restaurants</button>
 
             <div className="search-box">
                 <input type="text" name="search" placeholder="Search here..."/>
             </div>
             
-            <div style={{"display" : "flex"}}>
-
+            <div className="card-container" style={{"display" : "flex"}}>
+            
                 {
+                    
                     restList.map((restarant) => (
                         // console.log(restarant);
-                        <Card key={restarant.data.id} restData = {restarant}/>
+                        <Card key={restarant.cloudinaryId} restData = {restarant}/>
                     ))
+            
                 }
 
             </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,16 +5,19 @@ const cardStyle = {
 };
 
 const Card = (props) => {
-    console.log(props);
+    // console.log(props.restData.restaurantId);
     const {restData} = props;
-    const {name, cusine, rating, photo} = restData?.data;
-    // console.log(restData);
+    const {text, category, type, photo} = restData;
+
+    console.log({restData});
     return (
         <div className="card" style={cardStyle}>
-            <img src={photo} style={{"width" : "100%","height" : "40%", "alignSelf" : "center","display":"block"}}/>
-            <h4>{name}</h4>
-            <p><b>Cusine:</b> {cusine}</p>
-            <p><b>Rating:</b> {rating}</p>
+            {/* <img src={photo} style={{"width" : "100%","height" : "40%", "alignSelf" : "center","display":"block"}}/> */}
+            <img src="https://cdn.vectorstock.com/i/500p/22/07/burger-icon-isolated-flat-cartoon-sandwich-vector-15262207.jpg"
+            style={{"width" : "100%","height" : "40%", "alignSelf" : "center","border":"2px solid #17a864 "}}/>
+            <h4>{text}</h4>
+            <p><b>Category:</b> {category}</p>
+            <p><b>Type:</b> {type}</p>
         </div>
     )
 };
