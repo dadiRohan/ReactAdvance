2ecdaef047426eb70815fb707e81ebeba60bfa1b
Rohan Sable <39899398+dadiRohan@users.noreply.github.com>
2026-06-04 14:36:12 +0530

    card component addition

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,33 +1,55 @@
-import React from "react";
+import React, { useMemo } from "react";
 
+const FImages = [
+  'https://cdn.vectorstock.com/i/750p/85/10/pepperoni-pizza-vector-3268510.avif',
+  'https://cdn.vectorstock.com/i/500p/15/70/asian-food-platter-top-view-vector-58591570.avif',
+  'https://cdn.vectorstock.com/i/750p/00/76/tasty-burger-emblem-label-vector-11370076.avif',
+  'https://cdn.vectorstock.com/i/750p/36/36/english-breakfast-with-egg-and-sausages-tomatoes-vector-47863636.avif',
+  'https://cdn.vectorstock.com/i/750p/00/15/european-food-menu-cuisine-europe-meals-vector-29320015.avif',
+  'https://cdn.vectorstock.com/i/750p/09/61/asian-food-circle-diverse-cuisine-vector-50680961.avif',
+  'https://cdn.vectorstock.com/i/750p/84/56/american-breakfast-top-view-isolated-vector-13518456.avif'
+];
 
 const Card = (props) => {
-    const {restData} = props;
-    const {text, category, type, photo} = restData;
+    const { restData } = props;
+    const { text, category, type } = restData;
+
+    // Pick a random image once per component instance
+    const randomImage = useMemo(() => {
+        const randomIndex = Math.floor(Math.random() * FImages.length);
+        return FImages[randomIndex];
+    }, []);
 
     return (
-        <div className="inner-card" >
-            <img src="https://cdn.vectorstock.com/i/500p/22/07/burger-icon-isolated-flat-cartoon-sandwich-vector-15262207.jpg"
-            style={{"width" : "20%","height" : "40%", "alignSelf" : "center","border":"2px solid #17a864 "}}/>
+        <div className="inner-card">
+            <img 
+                src={randomImage}
+                style={{ 
+                    "width": "20%", 
+                    "height": "40%", 
+                    "alignSelf": "center", 
+                    "border": "2px solid #17a864" 
+                }}
+                alt="food"
+            />
             <h4>{text}</h4>
             <p><b>Category:</b> {category}</p>
             <p><b>Type:</b> {type}</p>
         </div>
-    )
+    );
 };
-
 
 export const withPromtedLabel = (Card) => {
     return (props) => {
         return (
-          <div>
-            <label className="absolute bg-black text-white m-2 p-2 rounded-lg">
-              Promoted
-            </label>
-            <Card {...props} />
-          </div>
+            <div>
+                <label className="absolute bg-black text-white m-2 p-2 rounded-lg">
+                    Promoted
+                </label>
+                <Card {...props} />
+            </div>
         );
-      };
-}
+    };
+};
 
 export default Card;
