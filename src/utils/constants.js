a1a8b860fa8db33a295ef3700080e8c08d5012a7
Rohan Sable <39899398+dadiRohan@users.noreply.github.com>
2026-06-15 17:32:52 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const LOGO_URL = "https://cdn.dribbble.com/userupload/46035326/file/d6585c272ba9c82b4078b16ff84503c8.png??resize=768x576&vertical=center";
+export const LOGO_URL = "https://static.vecteezy.com/system/resources/thumbnails/056/435/207/small/chef-mascot-logo-design-for-restaurant-cooking-man-yummy-express-food-illustration-vector.jpg";
 
